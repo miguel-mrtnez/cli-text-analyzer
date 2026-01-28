ae92456895a2d970e41c6e3c7fb788d58d895956
miguel-mrtnez <martinez.m@uc.cl>
2026-01-28 16:15:15 -0300

    feat: add --top logic

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
 use std::error::Error;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
-// use std::collections::HashMap;
+use std::collections::{BinaryHeap, HashMap};
+use std::cmp::Ordering;
 
 pub struct Config {
     flags: Flags,
@@ -13,7 +14,7 @@ struct Flags {
     words: bool,
     chars: bool,
     bytes: bool,
-    top: Option<u8>
+    top: Option<usize>
 }
 
 impl Flags {
@@ -38,18 +39,6 @@ impl Flags {
     }
 }
 
-// impl Default for Flags {
-//     fn default() -> Flags {
-//         Flags {
-//             lines: true,
-//             words: true,
-//             chars: true,
-//             bytes: false,
-//             top: None
-//         }
-//     }
-// }
-
 impl Config {
     pub fn build(mut args: impl Iterator<Item = String> + ExactSizeIterator) -> Result<Config, &'static str> {
         args.next();
@@ -66,7 +55,7 @@ impl Config {
                 "--c" | "--chars" => flags.chars = true,
                 "--b" | "--bytes" => flags.bytes = true,
                 "--t" | "--top" => {
-                    let n: u8 = args.next()
+                    let n: usize = args.next()
                         .expect("Missing argument for --top")
                         .parse()
                         .expect("Invalid argument for --top");
@@ -93,7 +82,7 @@ struct Stats {
     words: usize,
     chars: usize,
     bytes: usize,
-    // top: HashMap<String, usize>
+    top: HashMap<String, usize>
 }
 
 impl Stats {
@@ -103,27 +92,64 @@ impl Stats {
             words: 0,
             chars: 0,
             bytes: 0,
-            // top: HashMap::new()
+            top: HashMap::new()
         }
     }
 
-    fn print(&self, flags: Flags) {
+    fn print(&mut self, flags: Flags) {
         if flags.lines { println!("Lines: {}", self.lines); }
         if flags.words { println!("Words: {}", self.words); }
         if flags.chars { println!("Chars: {}", self.chars); }
         if flags.bytes { println!("Bytes: {}", self.bytes); }
-        // if flags.top.is_some() {
-        //     let n_top = flags.top.unwrap();
-        //     println!("Top {} words:", n_top);
-        // }
+        if flags.top.is_some() {
+            let n_top = flags.top.unwrap();
+            let mut heap: BinaryHeap<WordCount> = BinaryHeap::new();
+            
+            for (word, count) in self.top.drain() {
+                heap.push(WordCount{ word, count })
+            }
+
+            println!("Top {} words:", n_top);
+            for _ in 0..n_top {
+                if heap.is_empty() { return }
+                let wc = heap.pop().unwrap();
+                println!("{} {}", wc.count, wc.word);
+            }
+        }
     }
 }
 
-// fn count_words(line: &String) -> usize {
-//     1
-// }
+#[derive(Eq, PartialEq)]
+struct WordCount {
+    word: String,
+    count: usize,
+}
 
-// fn get_top_words
+impl Ord for WordCount {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.count
+            .cmp(&other.count)
+            .then_with(|| self.word.cmp(&other.word))
+    }
+}
+
+impl PartialOrd for WordCount {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+fn get_words_count(line: &String) -> usize {
+    line.split_whitespace().filter(|word| word.len() > 0 || word.is_ascii()).count()
+}
+
+fn count_words(line: &String, map: &mut HashMap<String, usize>) {
+    let words: Vec<&str> = line.split_whitespace().filter(|word| word.len() > 0 || word.is_ascii()).collect();
+    for word in words {
+        let count = map.entry(word.to_string()).or_insert(0);
+        *count += 1;
+    }
+}
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let mut stats = Stats::new();
@@ -132,14 +158,13 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
         let file = File::open(file).expect("Could not open file");
         let reader = BufReader::new(file);
 
-        // TODO: refactor, each flag is been checked at every iteration.
         for line in reader.lines() {
             let line = line?;
             if config.flags.lines { stats.lines += 1 }
-            // if config.flags.words { stats.words += count_words(&line) }
+            if config.flags.words { stats.words += get_words_count(&line) }
             if config.flags.chars { stats.chars += line.chars().count() }
             if config.flags.bytes { stats.bytes += line.bytes().count() }
-            // if config.flags.top {  }
+            if config.flags.top.is_some() { count_words(&line, &mut stats.top) }
         }
     }
 
@@ -149,10 +174,17 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 
 #[cfg(test)]
 mod tests {
-    // use super::*;
+    use super::*;
 
     #[test]
-    fn some_test() {
-        assert_eq!(2, 2);
+    fn get_words_count_zero() {
+        let result = get_words_count(&String::from(""));
+        assert_eq!(result, 0);
+    }
+
+    #[test]
+    fn get_words_count_two() {
+        let result = get_words_count(&String::from("a b"));
+        assert_eq!(result, 2);
     }
 }
